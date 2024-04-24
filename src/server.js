3256a17bcd67a5d18e0938c0b99f4a4d2c28f7b8
João Paulo <jpresendejava@gmail.com>
2024-04-24 11:39:23 -0400

    create metodo post

--- src/server.js
+++ src/server.js
@@ -1,8 +1,14 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+app.use(express.json());
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+
+
+app.post('/users',  (req, res)=> {
+    
+    const { name, email, password} = req.body;
+    res.send(name, email, password)
+  })
+  
 const PORT = 3333;
 app.listen(PORT, ()=>console.log(`Server is running on Port ${PORT}`) );
